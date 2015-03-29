e4c9c3e2db43f564a5ff203702c3591fc864d771
Jack Cook <jack@jackcook.nyc>
2015-03-29 10:18:58 -0400

    Render Genji's thing

--- Bluejay/NoteViewController.swift
+++ Bluejay/NoteViewController.swift
@@ -32,6 +32,21 @@ class NoteViewController: UIViewController, ATTSpeechServiceDelegate {
         self.speechButton.enabled = false
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "prepareSpeech", name: BJATTAuthenticatedNotification, object: nil)
+        
+        let url = NSURL(string: "http://10.31.67.43:5000/submit")
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "POST"
+        
+        let bodyParameters = [
+            "text": "George Washington (February 22, 1732 [O.S. February 11, 1731][Note 1][Note 2] – December 14, 1799) was the first President of the United States (1789–1797), the Commander-in-Chief of the Continental Army during the American Revolutionary War, and one of the Founding Fathers of the United States.[4] He presided over the convention that drafted the United States Constitution, which replaced the Articles of Confederation and remains the supreme law of the land.",
+        ]
+        let bodyString = self.stringFromQueryParameters(bodyParameters)
+        request.HTTPBody = bodyString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+            let content = NSString(data: data, encoding: NSUTF8StringEncoding)
+            self.renderMarkdown(content!)
+        }
     }
     
     override func viewDidLayoutSubviews() {
@@ -54,6 +69,8 @@ class NoteViewController: UIViewController, ATTSpeechServiceDelegate {
         let style = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)!
         let htmlStyle = "<style type=\"text/css\">\n" + style + "\n</style>"
         self.webView.loadHTMLString("<html><head>" + htmlStyle + "</head><body>" + source + "</body></html>", baseURL: nil)
+        
+        self.noticeLabel.alpha = 0
     }
     
     func prepareSpeech() {
@@ -104,5 +121,22 @@ class NoteViewController: UIViewController, ATTSpeechServiceDelegate {
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
+    }
+    
+    
+    func stringFromQueryParameters(queryParameters : Dictionary<String, String>) -> String {
+        var parts: [String] = []
+        for (name, value) in queryParameters {
+            var part = NSString(format: "%@=%@",
+                name.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!,
+                value.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)
+            parts.append(part)
+        }
+        return "&".join(parts)
+    }
+    
+    func NSURLByAppendingQueryParameters(URL : NSURL!, queryParameters : Dictionary<String, String>) -> NSURL {
+        let URLString : NSString = NSString(format: "%@?%@", URL.absoluteString!, self.stringFromQueryParameters(queryParameters))
+        return NSURL(string: URLString)!
     }
 }
